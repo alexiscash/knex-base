5f3d705b5bb71257fb0625c69edb7ab105c48867
alexiscash <alexiscash97@rocketmail.com>
2019-12-17 13:28:55 -0600

    added has many through func

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 
 let knex;
 
-class Base {   
+class Base {
     constructor(thang) {
         Object.assign(this, thang)
     }
 
 
-    static establishConnect(connection){
+    static establishConnection(connection) {
         knex = connection
     }
 
@@ -17,7 +17,7 @@ class Base {
 
     // find by id
     static async find(id) {
-        const [ thang ] = await knex(this.tableName + 's').where({ id: id });
+        const [thang] = await knex(this.tableName + 's').where({ id: id });
         return new this(thang);
     }
 
@@ -29,15 +29,15 @@ class Base {
 
     // takes an obj and creates a new record in db
     static async create(obj) {
-        const [ recordId ] = await knex(this.tableName + 's').insert(obj)
-        const [ record ] = await knex(this.tableName + 's').where({ id: recordId })
+        const [recordId] = await knex(this.tableName + 's').insert(obj)
+        const [record] = await knex(this.tableName + 's').where({ id: recordId })
         return new this(record);
     }
 
     // INSTANCE method that take obj and updates that record
     async update(obj) {
         await knex(this.constructor.name.toLowerCase() + 's').where({ id: this.id }).update(obj);
-        const [ record ] = await knex(this.constructor.name.toLowerCase() + 's').where({ id: this.id })
+        const [record] = await knex(this.constructor.name.toLowerCase() + 's').where({ id: this.id })
         return new this.constructor(record);
     }
 
@@ -46,18 +46,45 @@ class Base {
     }
 
     static belongsTo(name) {
-        this.prototype[name] = async function() {
-            const [ record ] = await knex(name.toLowerCase() + 's').where({ id: this[`${name}_id`] });
+        this.prototype[name] = async function () {
+            const [record] = await knex(name.toLowerCase() + 's').where({ id: this[`${name}_id`] });
             return record;
         }
     }
 
-    static hasMany(name) {
+    // static hasMany(name) {
+    //     this.prototype[name] = async function () {
+    //         const arr = await knex(name).where({ [`${this.constructor.tableName}_id`]: this.id });
+    //         return arr;
+    //     }
+    // }
+
+    static hasMany(name, opts = {}) {
+        if (opts.through) {
+            this.prototype[name] = async function() {
+                try {
+                    const arr = await knex(name)
+                            .innerJoin(opts.through, `${name}.id`, `${opts.through}.${name.substr(0, name.length -1)}_id`)
+                            .where({ [`${opts.through}.${this.constructor.tableName}_id`]: this.id })
+                            .select(`${name}.*`)
+                            .groupBy(`${name}.id`)
+                        return arr
+                } catch(err) {
+                    console.error('oh no', err);
+                }
+                
+            }
+            return 'ayy lmao';
+        }
+
         this.prototype[name] = async function() {
-            const arr = await knex(name).where({ [`${this.constructor.tableName}_id`]: this.id });
+            const arr = await knex(name).where({ [`${this.constructor.tableName}_id`]: this.id});
             return arr;
         }
     }
+
 }
+
+
 
 module.exports = Base
